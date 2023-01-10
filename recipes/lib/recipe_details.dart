b8f98ff8c3f789cd3fae05a60fdaa04d529a11cf
James A. Baker <tcodmos@gmail.com>
2023-01-10 18:01:32 +0300

    create work simple app

---
+++ recipes/lib/recipe_details.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:recipes/recipe.dart';
+
+class RecipeDetail extends StatefulWidget {
+  final Recipe recipe;
+
+  const RecipeDetail({super.key, required this.recipe});
+
+  @override
+  State<RecipeDetail> createState() => _RecipeDetailState();
+}
+
+class _RecipeDetailState extends State<RecipeDetail> {
+  int _sliderVal = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.recipe.label),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 300,
+              width: double.infinity,
+              child: Image(
+                image: AssetImage(widget.recipe.imageUrl),
+              ),
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+            Text(
+              widget.recipe.label,
+              style: const TextStyle(fontSize: 18),
+            ),
+            Expanded(
+              child: ListView.builder(
+                padding: const EdgeInsets.all(7.0),
+                itemCount: widget.recipe.ingredients.length,
+                itemBuilder: ((context, index) {
+                  final ingredient = widget.recipe.ingredients[index];
+                  final quantity = ingredient.quantity * _sliderVal;
+                  final measure = ingredient.measure;
+                  final name = ingredient.name;
+                  return Text('$quantity $measure $name');
+                }),
+              ),
+            ),
+            Slider(
+              min: 1,
+              max: 10,
+              divisions: 10,
+              label: '${_sliderVal * widget.recipe.servings} servings',
+              value: _sliderVal.toDouble(),
+              onChanged: ((value) {
+                setState(() {
+                  _sliderVal = value.round();
+                });
+              }),
+              activeColor: Colors.green,
+              inactiveColor: Colors.black,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
